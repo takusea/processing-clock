b47920e19215dbf10d01457f95d439ed3c0e8fa5
長澤 拓海 <53995265+takusea@users.noreply.github.com>
2021-10-19 15:38:59 +0900

    予定の取得の追加

--- scripts/script.js
+++ scripts/script.js
@@ -20,17 +20,61 @@ const openDialog = () => {
   menuDialog.classList.add('menu--open')
 }
 
+const sortEventList = () => {
+  eventList.sort((a, b) => {
+    const aDate = new Date(` ${a.startdate} ${a.starttime}`)
+    const bDate = new Date(` ${b.startdate} ${b.starttime}`)
+    if(aDate < bDate) {
+      return -1
+    }
+    if(bDate < aDate) {
+      return 1
+    }
+    return 0
+  })
+  console.log(eventList)
+}
+
+const getEventInfo = () => {
+  const nextEvent = eventList.find((event) => {
+    const nowDate = new Date()
+    const tenMinutesBeforeDate = new Date(nowDate.getTime() - 10 * 60000)
+    const eventDate = new Date(` ${event.startdate} ${event.starttime}`)
+    return tenMinutesBeforeDate < eventDate
+  })
+
+  if(nextEvent === undefined) return ('Event List is empty!')
+
+  const nowDate = new Date()
+  const nextEventDate = new Date(` ${nextEvent.startdate} ${nextEvent.starttime}`)
+  const nextEventTenMinutesLaterDate = new Date(nextEventDate.getTime() + 10 * 60000)
+
+  if(nowDate >= nextEventDate && nowDate < nextEventTenMinutesLaterDate) {
+    return ('Now Event is: ' + nextEvent.name)
+  }
+
+  return ('Next Event is: ' + nextEvent.name)
+}
+
+const changeEventInfoText = () => {
+  const eventInfoElement = document.getElementById('eventinfo')
+  eventInfoElement.innerHTML = getEventInfo()
+}
+
 const submitMenu = () => {
   const menuForm = document.getElementById('form')
   const event = {
     name: menuForm.name.value,
     type: menuForm.type.value,
-    date: menuForm.date.value,
-    time: menuForm.time.value,
+    startdate: menuForm.startdate.value,
+    starttime: menuForm.starttime.value,
+    finishdate: menuForm.finishdate.value,
+    finishtime: menuForm.finishtime.value,
     backgroundPath: menuForm.background.value,
     songPath: menuForm.song.value
   }
-  console.log(event)
+  eventList.push(event)
+  sortEventList()
 }
 
 const openDialogButton = document.getElementById('button-openmenu')
--- src/main.pde
+++ src/main.pde
@@ -12,6 +12,7 @@ void setup() {
 }
 
 void draw() {
+  changeEventInfoText();
   changeColor(nowWeek(), hour());
   background(backgroundColor);
   fill(foregroundColor);
@@ -19,32 +20,22 @@ void draw() {
   clock.update();
   
   if(isOffDuty(nowWeek(), hour())) {
-    clock.display(backgroundColor, foregroundColor);
+    clock.displaySimple(backgroundColor, foregroundColor);
   } else {
     clock.display(backgroundColor, foregroundColor);
   }
 }
 
 void isOffDuty(String week, int hour) {
-  return hour > 6 && hour < 18 || (week == "土" || week == "日")
+  return true ||hour < 6 && hour > 18 || (week == "土" || week == "日")
 }
 
 void changeColor(String week, int hour) {
-  if(week == "土" || week == "日") {
-    if(hour < 12) {
-      backgroundColor = #c2ff85;
-      foregroundColor = #333333;
-    } else {
-      backgroundColor = #694585;
-      foregroundColor = #ffffff;
-    }
+  if(isOffDuty(week, hour)) {
+    backgroundColor = #694585;
+    foregroundColor = #ffffff;
   } else {
-    if(hour < 12){
-      backgroundColor = #ffac54;
-      foregroundColor = #333333;
-    }else{
-      backgroundColor = #425f85;
-      foregroundColor = #ffffff;
-    }
+    backgroundColor = #c2ff85;
+    foregroundColor = #333333;
   }
 }
